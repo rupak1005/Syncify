3af4497cef4f205a50c6157510e11777f8d5db05
rupak1005 <rupak1005saini@gmail.com>
2025-07-18 14:24:30 +0530

    Implement production environment configuration for static file serving; update CORS settings to allow requests from the production URL and adjust base URL for chat socket connection.

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "http://localhost:3000",
+			origin: ["https://advancespotify.onrender.com","http://localhost:3000"],
 			credentials: true,
 		},
 	});
