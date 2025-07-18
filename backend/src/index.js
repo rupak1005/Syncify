f4d40467e773c1151aaf9bcb9ec01d920047549c
rupak1005 <rupak1005saini@gmail.com>
2025-07-18 14:37:15 +0530

    Remove production environment static file serving configuration from index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -97,9 +97,3 @@ httpServer.listen(PORT, () => {
 	connectDB();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
